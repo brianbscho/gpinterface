668f85c0230142fe47827479dfbfc38e9d83166b
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-18 15:20:14 +0900

    Update Head.tsx

--- frontend/components/Head.tsx
+++ frontend/components/Head.tsx
@@ -1,15 +1,8 @@
-import Head from "next/head";
 import Script from "next/script";
 
 export default function HeaderScripts() {
   return (
     <>
-      <Head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤔</text></svg>"
-        />
-      </Head>
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
