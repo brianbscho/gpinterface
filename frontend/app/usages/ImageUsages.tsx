a728153771e60b1e0e10430d956b1161e53733ad
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-18 16:43:50 +0900

    Show daily usage sum

--- frontend/app/usages/ImageUsages.tsx
+++ frontend/app/usages/ImageUsages.tsx
@@ -6,7 +6,7 @@ import callApi from "@/util/callApi";
 import { stringify } from "@/util/string";
 import { ImagePromptHistory } from "gpinterface-shared/type";
 import { ImageHistoriesGetResponse } from "gpinterface-shared/type/imageHistory";
-import { useCallback, useState } from "react";
+import { Fragment, useCallback, useMemo, useState } from "react";
 
 export default function ImageUsages() {
   const [imageHistories, setImageHistories] = useState<ImagePromptHistory[]>();
@@ -28,6 +28,26 @@ export default function ImageUsages() {
     }
   }, [lastHashId]);
 
+  const groupedImageHistories = useMemo(() => {
+    type HistoryWithPriceSum = {
+      [date: string]: { priceSum: number; histories: ImagePromptHistory[] };
+    };
+    const grouped = imageHistories?.reduce((acc: HistoryWithPriceSum, curr) => {
+      const date = curr.createdAt.split(" ")[0];
+      if (!acc[date]) {
+        acc[date] = { priceSum: 0, histories: [] };
+      }
+
+      acc[date].histories.push(curr);
+      acc[date].priceSum += curr.price;
+
+      return acc;
+    }, {});
+
+    if (!grouped) return undefined;
+    return Object.entries(grouped);
+  }, [imageHistories]);
+
   return (
     <List
       callApi={callImageHistoriesApi}
@@ -48,21 +68,35 @@ export default function ImageUsages() {
           </tr>
         </thead>
         <tbody className="w-full">
-          {imageHistories?.map((t) => (
-            <tr key={t.hashId} className="w-full">
-              <td className="text-nowrap">{t.model}</td>
-              <td className="truncate w-full">{t.prompt}</td>
-              <td className="truncate w-full">{stringify(t.input)}</td>
-              <td className="text-nowrap">${t.price}</td>
-              <td>
-                <picture>
-                  <img className="h-40" src={t.url} alt="ai_generated_image" />
-                </picture>
-              </td>
-              <td className="w-full">
-                <ImageUsage imageHistory={t} />
-              </td>
-            </tr>
+          {groupedImageHistories?.map(([date, history], index) => (
+            <Fragment key={date}>
+              {history.histories.map((t) => (
+                <tr key={t.hashId} className="w-full">
+                  <td className="text-nowrap">{t.model}</td>
+                  <td className="truncate w-full">{t.prompt}</td>
+                  <td className="truncate w-full">{stringify(t.input)}</td>
+                  <td className="text-nowrap">${t.price}</td>
+                  <td>
+                    <picture>
+                      <img
+                        className="h-40"
+                        src={t.url}
+                        alt="ai_generated_image"
+                      />
+                    </picture>
+                  </td>
+                  <td className="w-full">
+                    <ImageUsage imageHistory={t} />
+                  </td>
+                </tr>
+              ))}
+              {(spinnerHidden || index < groupedImageHistories.length) && (
+                <tr>
+                  <td className="font-bold text-lg">{date}</td>
+                  <td colSpan={4}>daily usage sum: ${history.priceSum}</td>
+                </tr>
+              )}
+            </Fragment>
           ))}
         </tbody>
       </table>
--- frontend/app/usages/TextUsages.tsx
+++ frontend/app/usages/TextUsages.tsx
@@ -6,7 +6,7 @@ import callApi from "@/util/callApi";
 import { stringify } from "@/util/string";
 import { TextPromptHistory } from "gpinterface-shared/type";
 import { TextHistoriesGetResponse } from "gpinterface-shared/type/textHistory";
-import { useCallback, useState } from "react";
+import { Fragment, useCallback, useMemo, useState } from "react";
 
 export default function TextUsages() {
   const [textHistories, setTextHistories] = useState<TextPromptHistory[]>();
@@ -28,6 +28,26 @@ export default function TextUsages() {
     }
   }, [lastHashId]);
 
+  const groupedTextHistories = useMemo(() => {
+    type HistoryWithPriceSum = {
+      [date: string]: { priceSum: number; histories: TextPromptHistory[] };
+    };
+    const grouped = textHistories?.reduce((acc: HistoryWithPriceSum, curr) => {
+      const date = curr.createdAt.split(" ")[0];
+      if (!acc[date]) {
+        acc[date] = { priceSum: 0, histories: [] };
+      }
+
+      acc[date].histories.push(curr);
+      acc[date].priceSum += curr.price;
+
+      return acc;
+    }, {});
+
+    if (!grouped) return undefined;
+    return Object.entries(grouped);
+  }, [textHistories]);
+
   return (
     <List
       callApi={callTextHistoriesApi}
@@ -47,16 +67,26 @@ export default function TextUsages() {
           </tr>
         </thead>
         <tbody className="w-full">
-          {textHistories?.map((t) => (
-            <tr key={t.hashId} className="w-full">
-              <td className="text-nowrap">{t.model}</td>
-              <td className="truncate w-full">{stringify(t.input)}</td>
-              <td className="truncate w-full">{t.content}</td>
-              <td className="text-nowrap">${t.price}</td>
-              <td className="text-nowrap">
-                <TextUsage textHistory={t} />
-              </td>
-            </tr>
+          {groupedTextHistories?.map(([date, history], index) => (
+            <Fragment key={date}>
+              {history.histories.map((t) => (
+                <tr key={t.hashId} className="w-full">
+                  <td className="text-nowrap">{t.model}</td>
+                  <td className="truncate w-full">{stringify(t.input)}</td>
+                  <td className="truncate w-full">{t.content}</td>
+                  <td className="text-nowrap">${t.price}</td>
+                  <td className="text-nowrap">
+                    <TextUsage textHistory={t} />
+                  </td>
+                </tr>
+              ))}
+              {(spinnerHidden || index < groupedTextHistories.length) && (
+                <tr>
+                  <td className="font-bold text-lg">{date}</td>
+                  <td colSpan={4}>daily usage sum: ${history.priceSum}</td>
+                </tr>
+              )}
+            </Fragment>
           ))}
         </tbody>
       </table>
