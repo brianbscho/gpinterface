c4509256c51f014e54cba2129dd14ebfb90123ee
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-19 15:06:22 +0900

    Update Write.tsx

--- frontend/components/general/buttons/Write.tsx
+++ frontend/components/general/buttons/Write.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@radix-ui/themes";
 import Link from "../links/Link";
 
-export default function Create() {
+export default function Write() {
   return (
     <Button asChild>
       <Link href="/thread/create">Write</Link>
