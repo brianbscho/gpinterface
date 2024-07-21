f2eec6e45c6853db2a4cd4702981c9f80d146284
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-21 21:37:50 +0900

    Create UserRequiredButton.tsx

---
+++ frontend/components/general/buttons/UserRequiredButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ReactNode, useCallback, useState } from "react";
+import Login from "../dialogs/Login";
+import useUserStore from "@/store/user";
+
+export default function UserRequiredButton({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick?: () => void;
+}) {
+  const { user } = useUserStore();
+  const [loginOpen, setLoginOpen] = useState(false);
+
+  const onClickUserRequired = useCallback(() => {
+    if (!user) {
+      setLoginOpen(true);
+      return;
+    }
+
+    if (onClick) {
+      onClick();
+    }
+  }, [onClick, user]);
+
+  return (
+    <>
+      <div onClick={onClickUserRequired} className="cursor-pointer">
+        {children}
+      </div>
+      <Login open={loginOpen} onClickLogin={() => setLoginOpen(false)} />
+    </>
+  );
+}
