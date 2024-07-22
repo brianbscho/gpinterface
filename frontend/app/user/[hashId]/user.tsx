7d976549c4589752752ccbf0f062b0f684fa7b07
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-22 16:36:38 +0900

    Add Text and Image tabs

--- frontend/app/user/[hashId]/user.tsx
+++ frontend/app/user/[hashId]/user.tsx
@@ -42,6 +42,7 @@ export default function User({ hashId }: { hashId: string }) {
           <Tabs.Trigger value="thread">Threads</Tabs.Trigger>
           <Tabs.Trigger value="post">Posts</Tabs.Trigger>
           <Tabs.Trigger value="bookmark">Bookmarks</Tabs.Trigger>
+          <Tabs.Trigger value="prompts">Bookmarked Prompts</Tabs.Trigger>
         </Tabs.List>
         <Tabs.Content value="thread">
           <Threads baseUrl={`/threads/user/${hashId}?type=thread`} />
@@ -51,6 +52,16 @@ export default function User({ hashId }: { hashId: string }) {
         </Tabs.Content>
         <Tabs.Content value="bookmark">
           <Threads baseUrl={`/threads/user/${hashId}?type=bookmark`} />
+        </Tabs.Content>
+        <Tabs.Content value="prompts">
+          <Tabs.Root defaultValue="text">
+            <Tabs.List>
+              <Tabs.Trigger value="text">Text</Tabs.Trigger>
+              <Tabs.Trigger value="image">Image</Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content value="text"></Tabs.Content>
+            <Tabs.Content value="image"></Tabs.Content>
+          </Tabs.Root>
         </Tabs.Content>
       </Tabs.Root>
     </div>
