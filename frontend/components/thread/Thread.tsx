311f27f0964ef11e9862f4e7ed0f9961f84af83e
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-30 13:56:14 +0900

    Update Thread.tsx

--- frontend/components/thread/Thread.tsx
+++ frontend/components/thread/Thread.tsx
@@ -1,22 +1,20 @@
 import { Thread as ThreadType } from "gpinterface-shared/type";
 import { FileText } from "lucide-react";
-import { Badge, Separator } from "../ui";
+import { Badge } from "../ui";
 
 export default function Thread({ thread }: { thread: ThreadType }) {
   const username = thread.user ? thread.user.name : "unknown";
   return (
-    <div className="py-1 border-b">
-      <div className="flex items-center gap-3">
-        {!thread.isPublic && <Badge>private</Badge>}
-        <div className="text-lg">{thread.title}</div>
+    <div className="py-3 border-b">
+      <div className="flex items-center">
+        {!thread.isPublic && <Badge className="mr-3">private</Badge>}
+        <div className="mr-3 text-sm md:text-base truncate">{thread.title}</div>
+        <FileText className="mr-1 shrink-0" />
+        <div className="text-xs md:text-sm">{thread.posts}</div>
       </div>
-      <div className="flex gap-3 justify-end items-center text-sm mt-1">
-        <FileText />
-        <div>{thread.posts}</div>
-        <Separator orientation="vertical" />
-        <div className="font-bold">{username}</div>
-        <Separator orientation="vertical" />
-        <div>{thread.createdAt}</div>
+      <div className="flex gap-3 items-center text-xs md:text-sm mt-1">
+        <div>{username}</div>
+        <div className="text-muted-foreground">{thread.createdAt}</div>
       </div>
     </div>
   );
