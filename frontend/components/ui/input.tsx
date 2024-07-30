ee72f46d93e699d2e11b9259cac3ad566fc92cd7
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-30 13:36:29 +0900

    Update focus-visible style of text input

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -12,7 +12,7 @@ const ShadcnInput = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
@@ -33,8 +33,11 @@ function Input({
     >;
   }) {
   return (
-    <div className="relative">
-      <ShadcnInput {...props} className={cn(props.className, "pl-10")} />
+    <div className="relative w-full">
+      <ShadcnInput
+        {...props}
+        className={cn(props.className, !Icon ? "" : "pl-10")}
+      />
       {!!Icon && (
         <span className="absolute start-0 inset-y-0 flex items-center justify-center px-2">
           <Icon />
