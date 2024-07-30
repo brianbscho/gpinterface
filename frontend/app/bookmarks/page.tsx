208579f4d3ba7f92bd14aa462d83d9edc042d354
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-31 00:42:14 +0900

    Simplify bookmarks page

--- frontend/app/bookmarks/page.tsx
+++ frontend/app/bookmarks/page.tsx
@@ -1,10 +1,18 @@
-import { Metadata } from "next";
-import Bookmarks from "./bookmarks";
+"use client";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return { title: "gpinterface - bookmarks" };
-}
+import Threads from "@/components/thread/Threads";
+import useUserStore from "@/store/user";
+import Login from "@/components/general/dialogs/Login";
 
-export default function Page() {
-  return <Bookmarks />;
+export default function Bookmarks() {
+  const { user } = useUserStore();
+
+  if (!user) {
+    return <Login open />;
+  }
+  return (
+    <div className="w-full max-w-7xl px-3">
+      <Threads baseUrl={`/threads/user/${user.hashId}?type=bookmark`} />
+    </div>
+  );
 }
