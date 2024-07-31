5600c49fc1c9b3e726d89b175f68afd47e942743
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-31 14:39:38 +0900

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,9 +4,7 @@ import type { Metadata } from "next";
 import Head from "@/components/Head";
 import Body from "@/components/Body";
 
-export const metadata: Metadata = {
-  title: "gpinterface",
-};
+export const metadata: Metadata = { title: "gpinterface" };
 
 export default function RootLayout({
   children,
