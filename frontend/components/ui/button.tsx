9501e8ff0c5609d08dc13ce22a1dd48089b21db4
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-31 14:35:44 +0900

    Update button layout for icon child

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -66,8 +66,13 @@ function Button({
 
   return (
     <ShadcnButton {...props} disabled={props.disabled || loading}>
-      {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-      {props.children}
+      {loading && <Loader2 className="h-4 w-4 animate-spin" />}
+      {loading && typeof props.children === "string" && (
+        <div className="mr-2"></div>
+      )}
+      {typeof props.children === "string" || !loading
+        ? props.children
+        : undefined}
     </ShadcnButton>
   );
 }
