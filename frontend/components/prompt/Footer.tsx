5390d0638917177cdb33e442ad7fb3a133320fc7
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-31 14:35:58 +0900

    Create Footer.tsx

---
+++ frontend/components/prompt/Footer.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Title from "../thread/Title";
+import { Button } from "../ui";
+import { Checkbox } from "../ui/checkbox";
+
+export default function Footer({
+  useIsPublic,
+  onClickCancel,
+  onClickCreate,
+  createText = "Create",
+  loading,
+}: {
+  useIsPublic: [boolean, (i: boolean) => void, boolean];
+  onClickCancel: () => void;
+  onClickCreate: () => void;
+  createText?: string;
+  loading: boolean;
+}) {
+  const [isPublic, setIsPublic, isEditable] = useIsPublic;
+
+  return (
+    <div className="w-full">
+      {isEditable && (
+        <div className="flex gap-3 md:gap-7">
+          <div className="flex gap-3 items-center w-32">
+            <Title>Public</Title>
+            <Checkbox
+              checked={isPublic}
+              onCheckedChange={(c) =>
+                typeof c === "boolean" ? setIsPublic(c) : undefined
+              }
+            />
+          </div>
+          <div className="text-xs md:leading-10 md:text-sm">
+            AI content can only be edited when it is set to private; public
+            prompt cannot be modified.
+          </div>
+        </div>
+      )}
+      <div className="flex justify-end gap-3">
+        <div>
+          <Button variant="outline" onClick={onClickCancel} loading={loading}>
+            Cancel
+          </Button>
+        </div>
+        <div>
+          <Button onClick={onClickCreate} loading={loading}>
+            {createText}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
