49fccf286ae7354540ad532a9042ee7a147548d8
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-07-31 16:55:01 +0900

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx,css}",
+    "./util/**/*.{js,ts,jsx,tsx,mdx,css}",
   ],
   prefix: "",
   theme: {
