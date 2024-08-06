cb1f866392e1d445f60de9234b6065f6b537af1d
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-06 15:34:24 +0900

    Move path of lib/utils to utils/css

--- frontend/components/ui/textarea.tsx
+++ frontend/components/ui/textarea.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/css";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
