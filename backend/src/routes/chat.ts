41d5d4e69a16e439fb1228d6b27f6334565f53f0
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-09 00:42:38 +0900

    Create /chat/duplicate API

--- backend/src/routes/chat.ts
+++ backend/src/routes/chat.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { createEntity } from "../util/prisma";
 import { ChatCreateResponse } from "gpinterface-shared/type/chat";
+import { Static } from "@sinclair/typebox";
+import { ParamSchema } from "gpinterface-shared/type";
+import { createChat } from "../controllers/chat";
 
 export default async function (fastify: FastifyInstance) {
   fastify.post("/", async (request, reply): Promise<ChatCreateResponse> => {
@@ -17,4 +20,44 @@ export default async function (fastify: FastifyInstance) {
       throw ex;
     }
   });
+  fastify.post<{ Params: Static<typeof ParamSchema> }>(
+    "/duplicate/:hashId",
+    { schema: { params: ParamSchema } },
+    async (request, reply): Promise<ChatCreateResponse> => {
+      try {
+        const { user } = await fastify.getUser(request, reply);
+        const { hashId } = request.params;
+
+        const oldChat = await fastify.prisma.chat.findFirst({
+          where: { hashId },
+          select: {
+            systemMessage: true,
+            contents: {
+              select: {
+                role: true,
+                content: true,
+                config: true,
+                modelHashId: true,
+              },
+              orderBy: { id: "asc" },
+            },
+          },
+        });
+        if (!oldChat) {
+          throw fastify.httpErrors.badRequest("chat is not available.");
+        }
+        const newChat = await createChat(fastify.prisma.chat, {
+          userHashId: user.hashId,
+          ...oldChat,
+        });
+        return newChat;
+      } catch (ex) {
+        console.error(
+          "path: /chat/duplicate/:hashId, method: post, error:",
+          ex
+        );
+        throw ex;
+      }
+    }
+  );
 }
