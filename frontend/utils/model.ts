c0ce3b549c260d1470e23b9409f60b5ed56433f2
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-13 13:42:49 +0900

    Create utils/model.ts

---
+++ frontend/utils/model.ts
@@ -0,0 +1,26 @@
+import { ConfigType } from "@/store/config";
+import { Model } from "gpinterface-shared/type";
+
+export const getApiConfig = (model: Model, config: ConfigType) => {
+  const newConfig: {
+    [key: string]: boolean | string | string[] | object | number;
+  } = {};
+
+  model.configs.forEach((c) => {
+    if (!config[c.name] || config[c.name] === c.default) return;
+
+    if (c.type === "boolean") {
+      newConfig[c.name] = config[c.name] === "true";
+    } else if (c.type === "object") {
+      newConfig[c.name] = JSON.parse(config[c.name]);
+    } else if (c.type === "array") {
+      newConfig[c.name] = config[c.name].split(",");
+    } else if (c.type === "integer" || c.type === "number") {
+      newConfig[c.name] = Number(config[c.name]);
+    } else {
+      newConfig[c.name] = config[c.name];
+    }
+  });
+
+  return newConfig;
+};
