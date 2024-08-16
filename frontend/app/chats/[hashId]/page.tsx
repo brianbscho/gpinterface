af79711492f212b0c30be31afe50ae70f4414bb3
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-16 12:39:53 +0900

    Create page.tsx

---
+++ frontend/app/chats/[hashId]/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Chat from "@/components/chat/Chat";
+import Provider from "@/components/chat/Provider";
+import callApi from "@/utils/callApi";
+import { Chat as ChatType } from "gpinterface-shared/type";
+import { useEffect, useState } from "react";
+
+export default function Page({ params }: { params: { hashId: string } }) {
+  const { hashId } = params;
+  const [chat, setChat] = useState<ChatType>();
+  useEffect(() => {
+    const callChatApi = async () => {
+      const response = await callApi<ChatType>({ endpoint: `/chat/${hashId}` });
+      if (response) {
+        setChat(response);
+      }
+    };
+    callChatApi();
+  }, []);
+
+  return (
+    <div className="w-full flex-1 grid grid-cols-[1fr_auto] gap-3 overflow-hidden">
+      <div className="h-full overflow-y-auto">
+        {!!chat && <Chat chat={chat} />}
+      </div>
+      <Provider />
+    </div>
+  );
+}
