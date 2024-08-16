47c9b8e2905c50b13631e15f94a3f24b2fc9cc7d
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-16 23:27:01 +0900

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Provider from "@/components/chat/Provider";
 
 export default function Page() {
   return (
-    <div className="w-full flex-1 grid grid-cols-[1fr_auto] gap-3 overflow-hidden">
+    <div className="w-full flex-1 grid grid-cols-[1fr_auto] overflow-hidden">
       <Chats />
       <Provider />
     </div>
