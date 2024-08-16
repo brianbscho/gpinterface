7441611eb6506ecbc4b1ff7ab7022fc1ee3a8a2a
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-16 12:39:48 +0900

    Create chat get API

--- backend/src/routes/chat.ts
+++ backend/src/routes/chat.ts
@@ -8,7 +8,7 @@ import {
   ChatUpdateResponse,
 } from "gpinterface-shared/type/chat";
 import { Static } from "@sinclair/typebox";
-import { ParamSchema } from "gpinterface-shared/type";
+import { Chat, ParamSchema } from "gpinterface-shared/type";
 import { createChat } from "../controllers/chat";
 import { getDateString } from "../util/string";
 import { getTextResponse } from "../util/text";
@@ -16,6 +16,62 @@ import { MILLION } from "../util/model";
 import { getTypedContent } from "../util/content";
 
 export default async function (fastify: FastifyInstance) {
+  fastify.get<{ Params: Static<typeof ParamSchema> }>(
+    "/:hashId",
+    { schema: { params: ParamSchema } },
+    async (request, reply): Promise<Chat> => {
+      try {
+        const { user } = await fastify.getUser(request, reply);
+        const { hashId } = request.params;
+
+        const chat = await fastify.prisma.chat.findFirst({
+          where: { hashId, userHashId: user.hashId },
+          select: {
+            hashId: true,
+            _count: { select: { apis: true, posts: true } },
+            posts: {
+              select: {
+                _count: {
+                  select: {
+                    likes: { where: { isLiked: true } },
+                  },
+                },
+              },
+            },
+            systemMessage: true,
+            contents: {
+              select: {
+                hashId: true,
+                model: { select: { hashId: true, name: true } },
+                role: true,
+                content: true,
+                config: true,
+              },
+              orderBy: { id: "asc" },
+            },
+            createdAt: true,
+          },
+          orderBy: { id: "desc" },
+          take: 5,
+        });
+        if (!chat) {
+          throw fastify.httpErrors.badRequest("chat is not available.");
+        }
+
+        const { _count, posts, createdAt, contents, ...rest } = chat;
+        return {
+          ...rest,
+          contents: contents.map((c) => getTypedContent(c)),
+          isApi: _count.apis > 0,
+          isPost: _count.posts > 0,
+          createdAt: getDateString(createdAt),
+        };
+      } catch (ex) {
+        console.error("path: /chat/:hashId, method: get, error:", ex);
+        throw ex;
+      }
+    }
+  );
   fastify.post<{ Body: Static<typeof ChatCreateSchema> }>(
     "/",
     { schema: { body: ChatCreateSchema } },
