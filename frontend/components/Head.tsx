3d043e030ab153913d3e04d2f54d59d57987261b
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-17 23:16:49 +0900

    Remove google ad id

--- frontend/components/Head.tsx
+++ frontend/components/Head.tsx
@@ -5,17 +5,6 @@ export default function Head() {
     <>
       <Script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
-      ></Script>
-      <Script
-        id={process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}
-      >{`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}');`}</Script>
-      <Script
-        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       ></Script>
       <Script id={process.env.NEXT_PUBLIC_GA_ID}>
