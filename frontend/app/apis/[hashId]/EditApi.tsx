661ec2c1243210a7b06d8e4e600e83128e8f7f90
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-19 00:37:11 +0900

    Add isPublic api call

--- frontend/app/apis/[hashId]/EditApi.tsx
+++ frontend/app/apis/[hashId]/EditApi.tsx
@@ -1,27 +1,51 @@
 "use client";
 
 import { Button, useToast } from "@/components/ui";
+import { Checkbox } from "@/components/ui/checkbox";
 import useContentStore from "@/store/content";
 import callApi from "@/utils/callApi";
 import { getApiConfig } from "@/utils/model";
-import { Checkbox } from "@radix-ui/react-checkbox";
+import { CheckedState } from "@radix-ui/react-checkbox";
 import { Static } from "@sinclair/typebox";
 import {
   ApiCreateResponse,
   ApiGetResponse,
   ApiUpdateSchema,
 } from "gpinterface-shared/type/api";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 type ApiType = ApiGetResponse;
 export default function EditApi({ api }: { api: ApiType | undefined }) {
-  const [isPublic, setIsPublic] = useState(api?.isPublic);
+  const [isPublic, setIsPublic] = useState<boolean>();
+  useEffect(() => {
+    setIsPublic(api?.isPublic);
+  }, [api?.isPublic]);
 
   const { toast } = useToast();
   const [config, model] = useContentStore((state) => [
     state.config,
     state.model,
   ]);
+  const onCheckedChange = useCallback(
+    async (c: CheckedState) => {
+      if (!api?.hashId || typeof c !== "boolean") return;
+
+      const response = await callApi<
+        ApiCreateResponse,
+        Static<typeof ApiUpdateSchema>
+      >({
+        endpoint: `/api/${api.hashId}`,
+        method: "PUT",
+        body: { isPublic: c },
+        showError: true,
+      });
+      if (response) {
+        toast({ title: "Saved!", duration: 1000 });
+        setIsPublic(c);
+      }
+    },
+    [api?.hashId, toast]
+  );
   const onClickDefault = useCallback(async () => {
     if (!model || !api?.hashId) return;
 
@@ -41,13 +65,11 @@ export default function EditApi({ api }: { api: ApiType | undefined }) {
 
   return (
     <>
-      <div className="mt-3 flex items-center gap-3">
+      <div className="py-2 pl-3 flex items-center gap-3">
         <Checkbox
           id="is_public"
           checked={isPublic}
-          onCheckedChange={(c) =>
-            setIsPublic(typeof c === "boolean" ? c : false)
-          }
+          onCheckedChange={onCheckedChange}
         />
         <label htmlFor="is_public" className="text-sm">
           Is this public API?
