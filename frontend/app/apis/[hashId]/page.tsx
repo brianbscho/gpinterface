849437ed1838ac6ee96515e6a324f4bbc59e53db
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-19 00:27:40 +0900

    Use EditApi component

--- frontend/app/apis/[hashId]/page.tsx
+++ frontend/app/apis/[hashId]/page.tsx
@@ -2,21 +2,16 @@
 
 import Api from "@/components/api/Api";
 import Provider from "@/components/chat/Provider";
-import { Button, Tabs, TabsList, TabsTrigger, useToast } from "@/components/ui";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui";
 import Chats from "./Chats";
 import Sessions from "./Sessions";
 import Document from "./Document";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import callApi from "@/utils/callApi";
-import { Static } from "@sinclair/typebox";
-import {
-  ApiCreateResponse,
-  ApiGetResponse,
-  ApiUpdateSchema,
-} from "gpinterface-shared/type/api";
-import useContentStore from "@/store/content";
-import { getApiConfig } from "@/utils/model";
+import { ApiGetResponse } from "gpinterface-shared/type/api";
 import { cn } from "@/utils/css";
+import useUserStore from "@/store/user";
+import EditApi from "./EditApi";
 
 export default function Page({ params }: { params: { hashId: string } }) {
   const { hashId } = params;
@@ -32,32 +27,16 @@ export default function Page({ params }: { params: { hashId: string } }) {
     callApiApi();
   }, [hashId]);
 
-  const { toast } = useToast();
-  const [config, model] = useContentStore((state) => [
-    state.config,
-    state.model,
-  ]);
-  const onClickDefault = useCallback(async () => {
-    if (!model) return;
-
-    const response = await callApi<
-      ApiCreateResponse,
-      Static<typeof ApiUpdateSchema>
-    >({
-      endpoint: `/api/${hashId}`,
-      method: "PUT",
-      body: { modelHashId: model.hashId, config: getApiConfig(model, config) },
-      showError: true,
-    });
-    if (response) {
-      toast({ title: "Saved!", duration: 1000 });
-    }
-  }, [hashId, model, config, toast]);
-
   const [tab, setTab] = useState("document");
   const isHidden = useCallback(
     (_tab: string) => (tab === _tab ? "" : "hidden"),
     [tab]
+  );
+
+  const userHashId = useUserStore((state) => state.user?.hashId);
+  const editable = useMemo(
+    () => !api?.userHashId || api?.userHashId === userHashId,
+    [api?.userHashId, userHashId]
   );
 
   return (
@@ -83,15 +62,9 @@ export default function Page({ params }: { params: { hashId: string } }) {
         </TabsList>
         <div className={cn("w-full flex-1 overflow-hidden", isHidden("api"))}>
           <div className="h-full grid grid-cols-[1fr_auto] overflow-hidden">
-            <Api api={api} />
+            <Api api={api} editable={editable} />
             <div className="flex flex-col w-full h-full overflow-hidden">
-              <Button
-                className="w-full rounded-none"
-                variant="outline"
-                onClick={onClickDefault}
-              >
-                Set as default model
-              </Button>
+              {editable && <EditApi api={api} />}
               <div className="flex-1 overflow-hidden">
                 <Provider />
               </div>
