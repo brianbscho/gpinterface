55f36fafeb7f32a81a8149ebd90563cf3f8c7d08
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-22 02:56:59 +0900

    Add icon variant to button

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        icon: "bg-primary text-secondary-foreground hover:bg-primary hover:text-primary-foreground justify-start",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
