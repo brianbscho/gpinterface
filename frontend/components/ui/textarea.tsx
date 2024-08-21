250b5820a681c796d23ec7632640dc6f05a930b6
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-21 17:43:44 +0900

    Update textarea CSS styles

--- frontend/components/ui/textarea.tsx
+++ frontend/components/ui/textarea.tsx
@@ -12,7 +12,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex max-h-96 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 resize-none",
+          "flex max-h-96 w-full rounded-md border border-input bg-neutral-700 text-white px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-yellow-300 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 resize-none",
           className
         )}
         ref={ref}
