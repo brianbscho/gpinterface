1fd5b20f8fdb9d006eeac440f60f44a478abbe1d
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-24 01:20:37 +0900

    Replace HoverCard to Tooltip

--- frontend/components/general/buttons/SmallHoverButton.tsx
+++ frontend/components/general/buttons/SmallHoverButton.tsx
@@ -1,15 +1,20 @@
-import { HoverCard, HoverCardTrigger, HoverCardContent } from "@/components/ui";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui";
 
 type Props = { children: React.ReactNode; message: string };
 export default function SmallHoverButton({ children, message }: Props) {
   return (
-    <HoverCard openDelay={0} closeDelay={100}>
-      <HoverCardTrigger asChild>
-        <div className="h-6">{children}</div>
-      </HoverCardTrigger>
-      <HoverCardContent className="text-sm w-auto px-3 py-1 border">
-        {message}
-      </HoverCardContent>
-    </HoverCard>
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild>
+          <div className="h-6">{children}</div>
+        </TooltipTrigger>
+        <TooltipContent>{message}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
