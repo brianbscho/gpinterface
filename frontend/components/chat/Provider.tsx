cd23046f9f3b9582acd03b785797104d30b6af5b
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-24 20:50:57 +0900

    Move Select model path

--- frontend/components/chat/Provider.tsx
+++ frontend/components/chat/Provider.tsx
@@ -1,11 +1,11 @@
-import SelectModel from "./SelectModel";
+import ModelSelect from "../selects/ModelSelect";
 import Model from "./Model";
 
 export default function Provider() {
   return (
     <div className="h-full overflow-hidden w-full md:w-[32rem]">
       <div className="absolute top-3 left-3 z-30">
-        <SelectModel />
+        <ModelSelect />
       </div>
       <div className="h-full overflow-y-auto pr-3">
         <Model />
