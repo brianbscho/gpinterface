fb53c954640a5a0638c8eae32a2b09751ff68b95
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-25 23:37:51 +0900

    Update Document.tsx

--- frontend/app/apis/[hashId]/Document.tsx
+++ frontend/app/apis/[hashId]/Document.tsx
@@ -6,6 +6,16 @@ import { ApiGetResponse } from "gpinterface-shared/type/api";
 import { Copy } from "lucide-react";
 import { ReactNode } from "react";
 
+type TitlePropt = { title: string; description: string };
+function Title({ title, description }: TitlePropt) {
+  return (
+    <div>
+      <Badge variant="tag">{title}</Badge>
+      <div className="text-neutral-400">{description}</div>
+    </div>
+  );
+}
+
 type ElementProp = { title: string; children: ReactNode };
 function Element({ title, children }: ElementProp) {
   return (
@@ -23,11 +33,11 @@ const CopyUrl = ({ url }: { url: string }) => {
     <div
       className="flex items-center underline gap-1 cursor-pointer"
       onClick={() => {
-        navigator.clipboard.writeText(`${url}`);
+        navigator.clipboard.writeText(url);
         toast({ title: "Copied!", duration: 1000 });
       }}
     >
-      {`${url}`}
+      {url}
       <Copy />
     </div>
   );
@@ -45,54 +55,48 @@ const Authentication = ({ userHashId }: { userHashId: string | null }) => {
 export default function Document({ api }: { api?: ApiGetResponse }) {
   const models = useModelStore((state) => state.models);
   const model = models.find((m) => m.hashId === api?.modelHashId);
-  if (!api) return null;
+  if (!api || !model) return null;
 
   return (
     <div className="md:pl-[9.5rem] px-3 pb-3 w-full h-full overflow-y-auto flex flex-col gap-7">
-      {!!model && (
-        <>
-          <div>
-            <Badge variant="tag">Info</Badge>
-            <div className="font-bold">{model.name}</div>
-          </div>
-          <Element title="Model config">
-            {Object.keys(api.config).length === 0
-              ? "Default"
-              : stringify(getApiConfig(model, api.config))}
-          </Element>
-          <Element title={api.isPublic ? "Public" : "Private"}>
-            {api.isPublic
-              ? "Accessible by anyone for testing and calling. Only the owner has editing privileges."
-              : "Only the owner can access, test, edit, and call this API."}
-          </Element>
-          <Element title="Share">
-            <CopyUrl
-              url={`${process.env.NEXT_PUBLIC_HOSTNAME}/apis/${api.hashId}`}
-            />
-          </Element>
-        </>
-      )}
       <div>
-        <Badge variant="tag">Chat Completion</Badge>
-        <div className="text-neutral-400">
-          Send a message that will be added to the end of a predefined messages
-          and receive an response for one-time chat interactions. Ideal for
-          isolated queries without session persistence.
-        </div>
+        <Badge variant="tag">Info</Badge>
+        <div className="font-bold">{model.name}</div>
       </div>
+      <Element title="Model config">
+        {Object.keys(api.config).length === 0
+          ? "Default"
+          : stringify(getApiConfig(model, api.config))}
+      </Element>
+      <Element title={api.isPublic ? "Public" : "Private"}>
+        {api.isPublic
+          ? "Accessible by anyone for testing and calling. Only the owner has editing privileges."
+          : "Only the owner can access, test, edit, and call this API."}
+      </Element>
+      <Element title="Share">
+        <CopyUrl
+          url={`${process.env.NEXT_PUBLIC_HOSTNAME}/apis/${api.hashId}`}
+        />
+      </Element>
+      <Title
+        title="Chat Completion"
+        description="Send a message that will be added to the end of a predefined messages
+          and receive an response for one-time chat interactions. Ideal for
+          isolated queries without session persistence."
+      />
       <Element title="POST">
         {process.env.NEXT_PUBLIC_SERVICE_ENDPOINT}/chat/completion
       </Element>
       <Authentication userHashId={api.userHashId} />
-      <Element title="Body">{`{apiHashId: "${api.hashId}", message: string}`}</Element>
+      <Element title="Body">
+        {`{apiHashId: "${api.hashId}", message: string}`}
+      </Element>
       <Element title="Response">{`{content: string}`}</Element>
-      <div>
-        <Badge variant="tag">Create Session</Badge>
-        <div className="text-neutral-400">
-          Initialize a new session and receive a hashed session ID, enabling
-          users to maintain a continuous conversation.
-        </div>
-      </div>
+      <Title
+        title="Create Session"
+        description="Initialize a new session and receive a hashed session ID, enabling
+          users to maintain a continuous conversation."
+      />
       <Element title="POST">
         {process.env.NEXT_PUBLIC_SERVICE_ENDPOINT}/session
       </Element>
@@ -101,14 +105,12 @@ export default function Document({ api }: { api?: ApiGetResponse }) {
         <div>{`{apiHashId: "${api.hashId}"}`}</div>
       </Element>
       <Element title="Response">{`{hashId: "SESSION_ID"}`}</Element>
-      <div>
-        <Badge variant="tag">Session Completion</Badge>
-        <div className="text-neutral-400">
-          Submit query within an active session, where it will be appended to
+      <Title
+        title="Session Completion"
+        description="Submit query within an active session, where it will be appended to
           the end of existing messages, to receive response that will be stored
-          within the session, allowing ongoing dialogue.
-        </div>
-      </div>
+          within the session, allowing ongoing dialogue."
+      />
       <Element title="POST">
         {process.env.NEXT_PUBLIC_SERVICE_ENDPOINT}/session/completion
       </Element>
@@ -117,20 +119,20 @@ export default function Document({ api }: { api?: ApiGetResponse }) {
         <div>{`{sessionHashId: "SESSION_ID", message: string}`}</div>
       </Element>
       <Element title="Response">{`{content: string}`}</Element>
-      <div>
-        <Badge variant="tag">Retrieve Session Messages</Badge>
-        <div className="text-neutral-400">
-          Retrieve the entire message history from a specific session.
-        </div>
-      </div>
+      <Title
+        title="Retrieve Session Messages"
+        description="Retrieve the entire message history from a specific session."
+      />
       <Element title="GET">
         {`${process.env.NEXT_PUBLIC_SERVICE_ENDPOINT}/session/{sessionHashId}/messages`}
       </Element>
       <Authentication userHashId={api.userHashId} />
       <Element title="Query Parameter">
-        <div>{`{sessionHashId: "SESSION_ID"}`}</div>
+        {`{sessionHashId: "SESSION_ID"}`}
       </Element>
-      <Element title="Response">{`{ messages: {role: string; content: string;}[] }`}</Element>
+      <Element title="Response">
+        {`{ messages: {role: string; content: string;}[] }`}
+      </Element>
     </div>
   );
 }
