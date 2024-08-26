0aea57b2a3b4bae0fa9ee4a1ebb1e3dbcbd1bc00
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-27 01:22:59 +0900

    Update top bar CSS

--- frontend/components/Body.tsx
+++ frontend/components/Body.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "next-themes";
 import MenusDropdown from "./dropdowns/MenusDropdown";
 import Link from "next/link";
 import { Toaster } from "@/components/ui/toaster";
+import NewGpiButton from "./buttons/NewGpiButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,22 +17,21 @@ export default function Body({
       <ThemeProvider attribute="class" forcedTheme="dark">
         <main className="h-screen flex flex-col items-center min-h-0 overflow-hidden">
           <div className="sticky top-0 left-0 w-full z-20 shrink-0 bg-background">
-            <div className="w-full border-b h-16">
-              <div className="w-full h-full px-3 flex gap-3 items-center">
+            <div className="w-full border-b">
+              <div className="w-full p-3 flex gap-3 items-center">
                 <Link href="/" className="font-bold text-lg">
-                  <div className="p-1 rounded-sm bg-foreground dark:bg-background">
+                  <div>
                     <picture>
                       <img
                         src="/logo.png"
                         alt="logo"
-                        height="27"
-                        width="117"
-                        className="block h-7 w-auto"
+                        className="block h-6 md:h-8 w-auto"
                       />
                     </picture>
                   </div>
                 </Link>
                 <div className="flex-1"></div>
+                <NewGpiButton />
                 <MenusDropdown />
               </div>
             </div>
