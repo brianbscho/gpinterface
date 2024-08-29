c963cf172ab347f57d9de1121bbfab260cb0357f
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-29 18:34:37 +0900

    Update top bar chat button

--- frontend/components/Body.tsx
+++ frontend/components/Body.tsx
@@ -3,8 +3,8 @@ import { ThemeProvider } from "next-themes";
 import MenusDropdown from "./dropdowns/MenusDropdown";
 import Link from "next/link";
 import { Toaster } from "@/components/ui/toaster";
-import { ShadcnButton } from "./ui";
-import { Plus } from "lucide-react";
+import { Button } from "./ui";
+import { CirclePlus } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,11 +32,17 @@ export default function Body({
                   </div>
                 </Link>
                 <div className="flex-1"></div>
-                <ShadcnButton asChild className="h-6 w-6 p-0 md:h-8 md:w-8">
-                  <Link href="/chats">
-                    <Plus className="h-4 w-4 md:h-5 md:w-5" />
-                  </Link>
-                </ShadcnButton>
+                <Link href="/chats">
+                  <Button
+                    className="bg-background p-0 pl-6 md:pl-8 h-6 md:h-8 w-24 md:w-28 border border-primary box-border relative"
+                    variant="icon"
+                  >
+                    <div className="h-6 md:h-8 w-6 md:w-8 p-1 md:p-1.5 rounded-md absolute -top-px -left-px bg-primary text-primary-foreground">
+                      <CirclePlus className="h-4 md:h-5 w-4 md:w-5" />
+                    </div>
+                    <div className="w-full text-xs md:text-sm">New chat</div>
+                  </Button>
+                </Link>
                 <MenusDropdown />
               </div>
             </div>
