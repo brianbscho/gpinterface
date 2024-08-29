08d86e88e88fe6de137ec0df67eaae4aadddafe0
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-30 03:42:06 +0900

    Create /gpis/search API

--- backend/src/routes/gpis.ts
+++ backend/src/routes/gpis.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { Static } from "@sinclair/typebox";
 import { getDateString } from "../util/string";
-import { QueryParamSchema } from "gpinterface-shared/type";
+import {
+  QueryParamSchema,
+  SearchQueryParamSchema,
+} from "gpinterface-shared/type";
 import {
   ContentHistorySelect,
   getIdByHashId,
@@ -71,6 +74,82 @@ export default async function (fastify: FastifyInstance) {
         };
       } catch (ex) {
         console.error("path: /gpis?lastHashId, method: get, error:", ex);
+        throw ex;
+      }
+    }
+  );
+  fastify.get<{ Querystring: Static<typeof SearchQueryParamSchema> }>(
+    "/search",
+    { schema: { querystring: SearchQueryParamSchema } },
+    async (request, reply): Promise<GpisGetResponse> => {
+      try {
+        const { user } = await fastify.getUser(request, reply, true);
+        const { keyword, lastHashId } = request.query;
+        const id = await getIdByHashId(
+          fastify.prisma.gpi.findFirst,
+          lastHashId
+        );
+        const search = keyword.split(" ").join(" | ");
+        console.log("🚀 ~ search:", search);
+
+        const gpis = await fastify.prisma.gpi.findMany({
+          where: {
+            ...(id > 0 && { id: { lt: id } }),
+            AND: [
+              { OR: [{ userHashId: user.hashId }, { isPublic: true }] },
+              {
+                OR: [
+                  { description: { search } },
+                  { chat: { systemMessage: { search } } },
+                  { chat: { contents: { some: { content: { search } } } } },
+                ],
+              },
+            ],
+          },
+          select: {
+            hashId: true,
+            userHashId: true,
+            description: true,
+            chat: {
+              select: {
+                hashId: true,
+                systemMessage: true,
+                contents: {
+                  select: {
+                    hashId: true,
+                    role: true,
+                    content: true,
+                    config: true,
+                    model: { select: { hashId: true, name: true } },
+                    histories: { select: ContentHistorySelect },
+                    isModified: true,
+                  },
+                },
+              },
+            },
+            config: true,
+            modelHashId: true,
+            isPublic: true,
+          },
+          orderBy: { id: "desc" },
+          take: 5,
+        });
+
+        return {
+          gpis: gpis.map((gpi) => {
+            const { chat, config, ...rest } = gpi;
+            return {
+              ...rest,
+              config: config as any,
+              chat: { ...chat, contents: getTypedContents(chat.contents) },
+            };
+          }),
+        };
+      } catch (ex) {
+        console.error(
+          "path: /gpis/search?keyword&lastHashId, method: get, error:",
+          ex
+        );
         throw ex;
       }
     }
