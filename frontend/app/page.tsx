2fd78ab24c4235b7278786f8cf0aec493a4da136
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-08-30 03:42:36 +0900

    Add baseUrl prop to Gpis

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,8 @@ import Gpis from "@/components/gpi/Gpis";
 
 export default function Page() {
   return (
-    <div className="w-full flex-1 grid grid-cols-[1fr_auto] overflow-hidden">
-      <Gpis />
+    <div className="w-full flex-1 overflow-hidden">
+      <Gpis baseUrl="/gpis?" />
     </div>
   );
 }
