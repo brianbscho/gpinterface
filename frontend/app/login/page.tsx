55a5a33020f771067fd75ad41a7f02e3fde90f5e
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-09-04 21:21:25 +0900

    Update login page alignment

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -115,126 +115,130 @@ export default function Page() {
   });
 
   return (
-    <div className="w-full max-w-sm px-3">
-      <div className="mt-20 w-full">
-        <GoogleLoginButton onClick={onClickGoogleLogin} />
-      </div>
-      <div className="my-12 flex items-center gap-3">
-        <div className="flex-1">
-          <Separator className="bg-theme" />
-        </div>
-        <div>OR</div>
-        <div className="flex-1">
-          <Separator className="bg-theme" />
-        </div>
-      </div>
-      <Tabs
-        className="w-full mt-12"
-        defaultValue="login"
-        onValueChange={(e) => setIsLogin(e === "login")}
-      >
-        <TabsList className="w-full">
-          <TabsTrigger value="login" className="flex-1">
-            Login
-          </TabsTrigger>
-          <TabsTrigger value="signup" className="flex-1">
-            Sign up
-          </TabsTrigger>
-        </TabsList>
-        <form onSubmit={onSubmit} noValidate>
-          <div className="mt-12">
-            <TabsContent value="signup">
-              <Input
-                type="text"
-                placeholder="Please type your username (no space)"
-                value={name}
-                onChange={(e) => setName(e.currentTarget.value)}
-                disabled={loading}
-                Icon={UserRound}
-              />
-              <div className="text-xs min-h-4 mt-1 mb-3 text-rose-500">
-                {name.length > 0 &&
-                  !nameValid &&
-                  "You can use only alphanumeric characters and -_,~!@#$^&*()+= special characters."}
-              </div>
-            </TabsContent>
-            <Input
-              type="email"
-              placeholder="email@domain.com"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-              disabled={loading}
-              Icon={Mail}
-            />
-            <div className="mt-1 text-xs h-4 text-rose-500">{emailMsg}</div>
-            <Input
-              type="password"
-              placeholder="Please use secure password"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-              className="mt-3"
-              disabled={loading}
-              Icon={Lock}
-            />
-            <div className="mt-1 text-xs h-4 text-rose-500">{passwordMsg}</div>
+    <div className="w-full flex-1 overflow-hidden">
+      <div className="w-full h-full overflow-y-auto">
+        <div className="w-full max-w-sm mx-auto min-h-full p-3 flex flex-col gap-12 justify-center items-center">
+          <GoogleLoginButton onClick={onClickGoogleLogin} />
+          <div className="w-full flex items-center gap-3">
+            <div className="flex-1">
+              <Separator className="bg-theme" />
+            </div>
+            <div>OR</div>
+            <div className="flex-1">
+              <Separator className="bg-theme" />
+            </div>
           </div>
-          <div>
-            <TabsContent value="login" className="mt-12">
-              <IconTextButton
-                className="w-full"
-                disabled={loginDisabled}
-                type="submit"
-                loading={loading}
-                text="Login"
-                Icon={UserRoundCheck}
-                size="large"
-              />
-            </TabsContent>
-            <TabsContent value="signup">
-              <div className="text-xs mt-1">
-                at least 8 characters long, at least one uppercase letter, at
-                least one lowercase letter, and at least one digit
-              </div>
-              <div className="flex items-center gap-3 mt-12">
-                <Checkbox
-                  id="agree"
-                  className="w-4 h-4"
-                  value=""
-                  checked={agree}
-                  onCheckedChange={(c) =>
-                    typeof c === "boolean" ? setAgree(c) : undefined
-                  }
+          <Tabs
+            className="w-full h-[30rem]"
+            defaultValue="login"
+            onValueChange={(e) => setIsLogin(e === "login")}
+          >
+            <TabsList className="w-full">
+              <TabsTrigger value="login" className="flex-1">
+                Login
+              </TabsTrigger>
+              <TabsTrigger value="signup" className="flex-1">
+                Sign up
+              </TabsTrigger>
+            </TabsList>
+            <form onSubmit={onSubmit} noValidate>
+              <div className="mt-12">
+                <TabsContent value="signup">
+                  <Input
+                    type="text"
+                    placeholder="Please type your username (no space)"
+                    value={name}
+                    onChange={(e) => setName(e.currentTarget.value)}
+                    disabled={loading}
+                    Icon={UserRound}
+                  />
+                  <div className="text-xs min-h-4 mt-1 mb-3 text-rose-500">
+                    {name.length > 0 &&
+                      !nameValid &&
+                      "You can use only alphanumeric characters and -_,~!@#$^&*()+= special characters."}
+                  </div>
+                </TabsContent>
+                <Input
+                  type="email"
+                  placeholder="email@domain.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.currentTarget.value)}
                   disabled={loading}
+                  Icon={Mail}
                 />
-                <label htmlFor="agree" className="text-xs">
-                  I agree to the&nbsp;
-                  <a
-                    href="https://www.termsfeed.com/live/0ce4dbce-17c2-4551-89c9-eb14fe206b71"
-                    target="_blank"
-                    className="underline"
-                  >
-                    privacy policy
-                  </a>
-                  &nbsp;and&nbsp;
-                  <a href="/terms" target="_blank" className="underline">
-                    terms and conditions
-                  </a>
-                </label>
+                <div className="mt-1 text-xs h-4 text-rose-500">{emailMsg}</div>
+                <Input
+                  type="password"
+                  placeholder="Please use secure password"
+                  value={password}
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                  className="mt-3"
+                  disabled={loading}
+                  Icon={Lock}
+                />
+                <div className="mt-1 text-xs h-4 text-rose-500">
+                  {passwordMsg}
+                </div>
               </div>
-              <div className="mt-3"></div>
-              <IconTextButton
-                className="w-full"
-                disabled={signupDisabled}
-                type="submit"
-                loading={loading}
-                text="Sign up"
-                Icon={UserRoundPlus}
-                size="large"
-              />
-            </TabsContent>
-          </div>
-        </form>
-      </Tabs>
+              <div>
+                <TabsContent value="login" className="mt-12">
+                  <IconTextButton
+                    className="w-full"
+                    disabled={loginDisabled}
+                    type="submit"
+                    loading={loading}
+                    text="Login"
+                    Icon={UserRoundCheck}
+                    size="large"
+                  />
+                </TabsContent>
+                <TabsContent value="signup">
+                  <div className="text-xs mt-1">
+                    at least 8 characters long, at least one uppercase letter,
+                    at least one lowercase letter, and at least one digit
+                  </div>
+                  <div className="flex items-center gap-3 mt-12">
+                    <Checkbox
+                      id="agree"
+                      className="w-4 h-4"
+                      value=""
+                      checked={agree}
+                      onCheckedChange={(c) =>
+                        typeof c === "boolean" ? setAgree(c) : undefined
+                      }
+                      disabled={loading}
+                    />
+                    <label htmlFor="agree" className="text-xs">
+                      I agree to the&nbsp;
+                      <a
+                        href="https://www.termsfeed.com/live/0ce4dbce-17c2-4551-89c9-eb14fe206b71"
+                        target="_blank"
+                        className="underline"
+                      >
+                        privacy policy
+                      </a>
+                      &nbsp;and&nbsp;
+                      <a href="/terms" target="_blank" className="underline">
+                        terms and conditions
+                      </a>
+                    </label>
+                  </div>
+                  <div className="mt-3"></div>
+                  <IconTextButton
+                    className="w-full"
+                    disabled={signupDisabled}
+                    type="submit"
+                    loading={loading}
+                    text="Sign up"
+                    Icon={UserRoundPlus}
+                    size="large"
+                  />
+                </TabsContent>
+              </div>
+            </form>
+          </Tabs>
+        </div>
+      </div>
     </div>
   );
 }
