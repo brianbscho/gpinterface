337e4c76e9a398791fbf259cd9f6b6cb84b420fd
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-09-05 13:52:30 +0900

    Create GpiDraft.tsx

--- frontend/app/gpis/user/page.tsx
+++ frontend/app/gpis/user/page.tsx
@@ -1,9 +1,71 @@
-import Gpis from "@/components/gpi/Gpis";
+"use client";
+
+import GpiTestDialog, {
+  TestDataType,
+} from "@/components/dialogs/GpiTestDialog";
+import Gpi from "@/components/gpi/Gpi";
+import GpiDraft from "@/components/gpi/GpiDraft";
+import List from "@/components/List";
+import useProviderTypes from "@/hooks/useProviderTypes";
+import callApi from "@/utils/callApi";
+import { ChatsGetResponse } from "gpinterface-shared/type/chat";
+import { useCallback, useState } from "react";
 
 export default function Page() {
+  const [chats, setChats] = useState<ChatsGetResponse>();
+  const [lastHashId, setLastHashId] = useState("");
+  const [spinnerHidden, setSpinnerHidden] = useState(false);
+
+  const callChatsApi = useCallback(async () => {
+    const response = await callApi<ChatsGetResponse>({
+      endpoint: `/chats/user?lastHashId=${lastHashId}`,
+      showError: true,
+    });
+    if (response) {
+      setChats((prev) => [...(prev ?? []), ...response]);
+      if (response.length === 0) {
+        setSpinnerHidden(true);
+      }
+    }
+  }, [lastHashId]);
+
+  useProviderTypes();
+
+  const [testData, setTestData] = useState<TestDataType>();
+  const [testOpen, setTestOpen] = useState(false);
+
   return (
-    <div className="w-full flex-1 overflow-hidden">
-      <Gpis baseUrl="/gpis/user?" />
+    <div className="w-full h-full overflow-hidden relative">
+      <div className="h-full w-full overflow-y-auto">
+        <div className="w-full max-w-4xl mx-auto flex flex-col gap-3 p-3">
+          <List
+            callApi={callChatsApi}
+            emptyMessage={"Start your chat!"}
+            elements={chats}
+            spinnerHidden={spinnerHidden}
+            useLastHashId={[lastHashId, setLastHashId]}
+          >
+            {chats?.map((chat) => {
+              const { gpis, userHashId, ...rest } = chat;
+              if (gpis.length > 0) {
+                return (
+                  <Gpi
+                    key={chat.hashId}
+                    gpi={{ ...gpis[0], userHashId, chat: rest }}
+                    setTestData={setTestData}
+                    setTestOpen={setTestOpen}
+                  />
+                );
+              }
+              return <GpiDraft key={chat.hashId} chat={chat} />;
+            })}
+          </List>
+        </div>
+      </div>
+      <GpiTestDialog
+        useTestData={[testData, setTestData]}
+        useTestOpen={[testOpen, setTestOpen]}
+      />
     </div>
   );
 }
---
+++ frontend/components/gpi/GpiDraft.tsx
@@ -0,0 +1,44 @@
+import IconTextButton from "../buttons/IconTextButton";
+import { CircleX, FileCog } from "lucide-react";
+import Link from "next/link";
+import ContentStatic from "@/components/content/ContentStatic";
+import { ChatGetResponse } from "gpinterface-shared/type/chat";
+
+type Props = { chat: ChatGetResponse };
+export default function GpiDraft({ chat }: Props) {
+  return (
+    <div className="w-full border border-theme rounded-md pt-3">
+      <div className="sticky top-0 rounded-md w-full px-3 py-3 grid grid-cols-3 md:flex gap-3 bg-background z-30">
+        <div className="flex-1 md:flex-initial md:w-32">
+          <Link href={`/chats/${chat.hashId}`}>
+            <IconTextButton
+              className="w-full md:w-32"
+              Icon={FileCog}
+              text="Edit"
+              responsive
+            />
+          </Link>
+        </div>
+        <div className="flex-1 md:flex-initial md:w-32">
+          <IconTextButton
+            className="w-full md:w-32"
+            Icon={CircleX}
+            variant="icon_destructive"
+            text="Delete"
+            responsive
+          />
+        </div>
+      </div>
+      <div className="w-full px-3">
+        <div className="flex flex-col gap-3 mb-3">
+          {chat.systemMessage.length > 0 && (
+            <ContentStatic role="system" content={chat.systemMessage} />
+          )}
+          {chat.contents.map((content) => (
+            <ContentStatic key={content.hashId} {...content} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
