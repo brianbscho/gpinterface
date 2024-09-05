e5c80122a3e976955c9039d2dec559f5eea238e1
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-09-06 03:30:30 +0900

    Update ContentStatic design

--- frontend/components/content/ContentStatic.tsx
+++ frontend/components/content/ContentStatic.tsx
@@ -1,5 +1,3 @@
-import { Badge, CardContent, CardDescription } from "../../components/ui";
-
 type ContentStaticProps = {
   role: string;
   content: string;
@@ -9,23 +7,20 @@ type ContentStaticProps = {
 export default function ContentStatic({ ...props }: ContentStaticProps) {
   const { role, model, isModified, content } = props;
   return (
-    <CardContent className="p-0">
+    <>
       <div className="flex items-center gap-1">
-        {role !== "assistant" && <Badge variant="tag">{role}</Badge>}
-        {role === "assistant" && (
-          <Badge variant="tag">{!model ? "assistant" : model.name}</Badge>
-        )}
+        <div className="text-sm pt-2">
+          {role !== "assistant" ? role : !model ? "assistant" : model.name}
+        </div>
         {isModified === true && (
           <div className="ml-1 text-xs self-start">*answer modified</div>
         )}
       </div>
-      <CardDescription>
-        <div className="relative mt-3">
-          <div className="whitespace-pre-wrap px-3 py-2 text-base border rounded-md">
-            <div className="min-h-6">{content}</div>
-          </div>
+      <div className="relative">
+        <div className="whitespace-pre-wrap px-3 py-2 text-base border rounded-md bg-neutral-700">
+          <div className="min-h-6 text-neutral-300 text-sm">{content}</div>
         </div>
-      </CardDescription>
-    </CardContent>
+      </div>
+    </>
   );
 }
