6fb634fe602684223d3f1a8dd00650913bfbd3a2
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-09-06 03:28:31 +0900

    Create GpiDropdown.tsx

---
+++ frontend/components/dropdowns/GpiDropdown.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Copy, CircleEllipsis, LinkIcon, FileCog } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  useToast,
+} from "../ui";
+import IconButton from "../buttons/IconButton";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import callApi from "@/utils/callApi";
+import { GpiCreateResponse } from "gpinterface-shared/type/gpi";
+import { Static } from "@sinclair/typebox";
+import { ParamSchema } from "gpinterface-shared/type";
+import useUserStore from "@/store/user";
+import Link from "next/link";
+
+export default function GpiDropdown({ gpiHashId }: { gpiHashId: string }) {
+  const { toast } = useToast();
+  const onClickShare = useCallback(() => {
+    navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_HOSTNAME}/gpis/${gpiHashId}`
+    );
+    toast({ title: "Copied!", duration: 1000 });
+  }, [gpiHashId, toast]);
+
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const onClickCopy = useCallback(async () => {
+    setLoading(true);
+    const response = await callApi<
+      GpiCreateResponse,
+      Static<typeof ParamSchema>
+    >({
+      endpoint: `/gpi/copy`,
+      method: "POST",
+      body: { hashId: gpiHashId },
+      showError: true,
+    });
+    if (response) {
+      router.push("/gpis/user");
+    } else {
+      setLoading(false);
+    }
+  }, [gpiHashId, router]);
+
+  const userHashId = useUserStore((state) => state.user?.hashId);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <IconButton Icon={CircleEllipsis} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-auto px-1">
+        <DropdownMenuItem>
+          <Link href={`/gpis/${gpiHashId}/edit`}>
+            <div className="flex gap-3">
+              <FileCog className="h-3 w-3 md:h-4 md:w-4" />
+              <span className="text-xs md:text-sm">Edit</span>
+            </div>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="gap-3" onClick={onClickShare}>
+          <LinkIcon className="h-3 w-3 md:h-4 md:w-4" />
+          <span className="text-xs md:text-sm">Share</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="gap-3"
+          onClick={onClickCopy}
+          disabled={loading}
+        >
+          <Copy className="h-3 w-3 md:h-4 md:w-4" />
+          <span className="text-xs md:text-sm">Make copy</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
