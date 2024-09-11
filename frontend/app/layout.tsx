660383e638f53b9b6ab74418f8f1b8d4f56a1c31
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-09-11 14:56:12 +0900

    [09112024] Add metadata and google ads tracking

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,7 +6,11 @@ import Head from "@/components/Head";
 import Body from "@/components/Body";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-export const metadata: Metadata = { title: "gpinterface" };
+export const metadata: Metadata = {
+  title: "gpinterface | create API for your prompts",
+  description:
+    "Instantly deploy your generative prompts with our easy-to-use interface, enabling seamless integration to multiple LLMs.",
+};
 
 export default function RootLayout({
   children,
--- frontend/components/Head.tsx
+++ frontend/components/Head.tsx
@@ -14,6 +14,17 @@ export default function Head() {
 
         gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`}
       </Script>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
+      ></Script>
+      <Script id={process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}');`}
+      </Script>
     </>
   );
 }
